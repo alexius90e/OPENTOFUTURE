fbafb9979052936084681dbc3b2d9596ac198ded
alexius90e <alexius90e@mail.ru>
2021-02-24 01:02:10 +0300

    feat: add localization

--- js/main.js
+++ js/main.js
@@ -2,13 +2,42 @@ const downloadsContainer = document.querySelector('main .downloads');
 const loadMoreButton = document.querySelector('.load-button');
 const take = 5;
 const skip = skipCounter();
+const lang = document.querySelector('html').getAttribute('lang');
+const langTitle = document.querySelector('.lang-tittle');
+
+const dict = {
+    en : {
+        month : ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        moreFiles : 'More files',
+    },
+    ru : {
+        month : ['Января','Февраля', 'Марта', 'Апреля', 'Мая', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'],
+        moreFiles : 'Еще файлы',
+    }
+}
+
+
+langTitle.innerHTML = lang;
+document.querySelector('.lang-wrapper').addEventListener('click', function(event){
+    const langPanel = document.querySelector('.lang-dropdown');
+    if (langPanel.classList.contains('dropdown')){
+        langPanel.classList.remove('dropdown');
+    } else {
+        langPanel.classList.add('dropdown');
+    }
+});
+
+document.body.addEventListener('click', function(event){
+    if (!event.target.closest('.lang-wrapper')) {
+        document.querySelector('.lang-dropdown').classList.remove('dropdown');
+    }
+});
 
 makeResponse();
 loadMoreButton.addEventListener('click', () => {
     makeResponse();
     activateMoreButton();
 })
-
 
 
 function skipCounter() {
@@ -49,10 +78,10 @@ function createCard(obj){
                 <div class="item__title">${counter + 1}.    ${obj.files[variable][0]}</div>
                 <div class="item__size">${(obj.files[variable][1] / 1024**2 + 0.05).toFixed(1)} Mb</div>
                 <button class="item__copy-button">
-                    <img src="./images/copy-link-icon.svg" alt="copy-link-icon">
+                    <img src="/images/copy-link-icon.svg" alt="copy-link-icon">
                 </button>
                 <a class="item__download-button" href="${linkStart + variable}">
-                    <img src="./images/fluent_arrow-icon.svg" alt="copy-link-icon">
+                    <img src="/images/fluent_arrow-icon.svg" alt="copy-link-icon">
                 </a>
             </div>
         `;
@@ -83,11 +112,9 @@ function activateMoreButton() {
         item.addEventListener('click', function() {
             let hiddenItems = this.parentNode.parentNode.querySelector('.downloads-body__hidden');
             if (hiddenItems.classList.contains('hide')){
-                // hiddenItems.classList.remove('d-none');
                 hiddenItems.classList.remove('hide');
             } else {
                 hiddenItems.classList.add('hide')
-                // hiddenItems.classList.add('d-none');
             }
         });
     });
@@ -96,7 +123,7 @@ function activateMoreButton() {
 function addDate(obj){
     let currDate = new Date(obj.time * 1000);
 
-    let month = ['January','February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    let month = dict[lang].month;
     month = month[currDate.getMonth()];
     let day = `${currDate.getDate() / 10 >= 1 ? currDate.getDate() : '0' + currDate.getDate()} ${month} ${currDate.getFullYear()}`;
     let hours = currDate.getHours() / 10 >= 1 ? currDate.getHours() : '0' + currDate.getHours();
@@ -113,7 +140,7 @@ function addDownloadItem(obj, downloadsCard){
     if (Object.keys(obj.files).length > 4) {
         let showMoreButton = document.createElement('button');
         showMoreButton.classList.add('more-files__button');
-        showMoreButton.innerText = 'More files';
+        showMoreButton.innerText = dict[lang].moreFiles;
         let downloadsCardFooter = document.createElement('div');
         downloadsCardFooter.classList.add('downloads-footer');
         downloadsCard.appendChild(downloadsCardFooter);
